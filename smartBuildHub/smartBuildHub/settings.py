16179eabb4c1366cf7dd2f8b76cccf7cef9eb662
arshad033 <arshadobra85@gmail.com>
2025-04-17 02:16:53 +0530

    Add Tailwind CSS integration with PostCSS setup
    - Created package.json for managing dependencies and scripts for building and developing Tailwind CSS styles.
    - Added postcss.config.js to configure PostCSS with Tailwind CSS, postcss-simple-vars, and postcss-nested plugins.
    - Introduced styles.css to import Tailwind CSS and define a source path for scanning Django template files.
    - Created base.html template to demonstrate the integration of Django with Tailwind CSS, including necessary meta tags and a sample layout.

--- smartBuildHub/smartBuildHub/settings.py
+++ smartBuildHub/smartBuildHub/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'theme'
 ]
-
+TAILWIND_APP_NAME = 'theme'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -121,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+
