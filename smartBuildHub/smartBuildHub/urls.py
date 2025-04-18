19894b282ff503dd4befa90fc58765fea82c6be1
arshad033 <arshadobra85@gmail.com>
2025-04-18 15:58:17 +0530

    Refactor URL configuration and enhance CSS styles
    - Updated the URL configuration in `urls.py` to ensure proper inclusion of the `django_browser_reload` URLs.
    - Made extensive modifications to `styles.css`, including:
      - Added new color variables and adjusted existing ones for better theming.
      - Introduced new spacing, sizing, and typography utilities.
      - Enhanced layout classes for better responsiveness and grid support.
      - Improved hover and focus states for various elements.
      - Added support for new backdrop and shadow utilities.
      - Implemented color-mix for dynamic background and border colors.

--- smartBuildHub/smartBuildHub/urls.py
+++ smartBuildHub/smartBuildHub/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('mainapp.mainappurls')),
 
-    path("_reload_/", include("django_browser_reload.urls"))
+    path("_reload_/", include("django_browser_reload.urls")),
 ]
