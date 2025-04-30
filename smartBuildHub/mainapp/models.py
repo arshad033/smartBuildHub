619f13303ca4d34266166f9d6a11fd8c2e1d85b8
anuj407 <anuj97953a@gmail.com>
2025-04-30 10:23:18 +0530

    feat: add User model with user types and string representation method

--- smartBuildHub/mainapp/models.py
+++ smartBuildHub/mainapp/models.py
@@ -14,3 +14,21 @@ class LoginInfo(models.Model):
     username = models.CharField(max_length=100,unique=True)
     password = models.CharField(max_length=256)
 
+class User(models.Model):
+    USER_TYPES = (
+        ('supplier', 'Supplier'),
+        ('contractor', 'Contractor'),
+        ('homeOwner', 'HomeOwner'),
+        ('admin', 'Admin'),
+        ('architecture', 'Architecture'),  
+        # Add more types if needed
+    )
+
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)  # Normally hashed, so 128 is safe
+    usertype = models.CharField(max_length=20, choices=USER_TYPES)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.user_type})"
