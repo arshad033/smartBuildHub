53dc6483f3cf43a5785fe1377c3171d657d0ec0e
anuj407 <anuj97953a@gmail.com>
2025-04-30 10:23:12 +0530

    feat: add user registration to admin panel and update URL patterns for improved navigation

--- smartBuildHub/adminapp/adminappurls.py
+++ smartBuildHub/adminapp/adminappurls.py
@@ -5,5 +5,10 @@ from . import views
 urlpatterns = [
    path('admindash/',views.admindash,name='admindash'),
    path('viewenq/',views.viewenq,name='viewenq'),
+   path('viewHomeOwners/',views.viewHomeOwners,name='viewHomeOwners'),
+   path('viewSuppliers/',views.viewSuppliers,name='viewSuppliers'),
+   path('viewArchitecture/', views.viewArchitecture, name='viewArchitecture'),
+   path('viewContractor/',views.viewContractor,name='viewContractor'),
+   path('view_users/',views.view_users,name='view_users'),
    path('adminlogout/',views.adminlogout,name='adminlogout'),
 ]
--- smartBuildHub/mainapp/admin.py
+++ smartBuildHub/mainapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Enquiry,LoginInfo
+from .models import Enquiry,LoginInfo , User
 # Register your models here.
 admin.site.register(Enquiry)
 admin.site.register(LoginInfo)
+admin.site.register(User)
