604433f6d115cdcb5e205377d9a46f7c9cb416cc
anuj407 <anuj97953a@gmail.com>
2025-04-30 10:23:03 +0530

    feat: add user model and implement views for managing users, contractors, suppliers, and architectures with session validation

--- smartBuildHub/adminapp/views.py
+++ smartBuildHub/adminapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from mainapp.models import Enquiry
+from mainapp.models import Enquiry , User
 from django.contrib import  messages
 
 # Create your views here.
@@ -35,4 +35,64 @@ def viewenq(request):
         messages.error(request,'Login first')
         return redirect('login')
     
+def viewHomeOwners(request):
+        if 'adminid' in request.session:
+            adminid = request.session.get('adminid')
+            return render(request,'viewenquiries.html')
+        else:
+            messages.error(request,'Login first')
+            return redirect('login')
+        
+        
+def view_users(request):
+    if 'adminid' in request.session:
+        adminid = request.session.get('adminid')
+        users = User.objects.all()
+        context = {
+            'users': users,
+            'adminid': adminid
+        }
+        return render(request, 'viewusers.html', context)
+    else:
+        messages.error(request, 'Login first')
+        return redirect('login')
     
+def viewContractor(request):
+    if 'adminid' in request.session:
+        adminid = request.session.get('adminid')
+        contractors = User.objects.filter(user_type='contractor')
+        context = {
+            'contractors': contractors,
+            'adminid': adminid
+        }
+        return render(request, 'viewcontractors.html', context)
+    else:
+        messages.error(request, 'Login first')
+        return redirect('login')
+
+
+def viewSuppliers(request):
+    if 'adminid' in request.session:
+        adminid = request.session.get('adminid')
+        suppliers = User.objects.filter(user_type='supplier')
+        context = {
+            'suppliers': suppliers,
+            'adminid': adminid
+        }
+        return render(request, 'viewsuppliers.html', context)
+    else:
+        messages.error(request, 'Login first')
+        return redirect('login')
+    
+def viewArchitecture(request):
+    if 'adminid' in request.session:
+        adminid = request.session.get('adminid')
+        architectures = User.objects.filter(user_type='architecture')
+        context = {
+            'architectures': architectures,
+            'adminid': adminid
+        }
+        return render(request, 'viewarchitecture.html', context)
+    else:
+        messages.error(request, 'Login first')
+        return redirect('login')
