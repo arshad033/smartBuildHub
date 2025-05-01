ba8eca402adbad18fe2b78e8d50d842cca275c55
arshad033 <arshadobra85@gmail.com>
2025-05-01 14:01:05 +0530

    feat: remove unused view_users route from adminapp URLs

--- smartBuildHub/adminapp/adminappurls.py
+++ smartBuildHub/adminapp/adminappurls.py
@@ -9,6 +9,5 @@ urlpatterns = [
    path('viewSuppliers/',views.viewSuppliers,name='viewSuppliers'),
    path('viewArchitecture/', views.viewArchitecture, name='viewArchitecture'),
    path('viewContractor/',views.viewContractor,name='viewContractor'),
-   path('view_users/',views.view_users,name='view_users'),
    path('adminlogout/',views.adminlogout,name='adminlogout'),
 ]
